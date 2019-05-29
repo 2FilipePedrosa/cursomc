6a59ed0a3301df8fc5bec84de749d66c946fea4e
Filipe Pedrosa <f.filipe.pedrosa@gmail.com>
2019-05-29 12:18:24 +0100

    Criando de repository e Service para Categoria

---
+++ src/main/java/com/filipe/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.filipe.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.filipe.cursomc.domain.Categoria;
+import com.filipe.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+ public Categoria buscar(Integer id) {
+	 Optional <Categoria> obj = repo.findById(id);
+	 return obj.orElse(null);
+	  
+ }
+ 
+}
